0abf83a30ac6c39187a7fe80fb51ed24cb5a3862
Danilo Augusto <dandevweb@gmail.com>
2024-07-25 20:51:28 -0300

    feat: add relationships and casts in models

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Enums\Enums\AddressTypeEnum;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Address extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'type' => AddressTypeEnum::class,
+    ];
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
 }
--- app/Models/ClassModel.php
+++ app/Models/ClassModel.php
@@ -2,25 +2,21 @@
 
 namespace App\Models;
 
+use App\Enums\Enums\ShiftEnum;
 use App\Enums\GradeEnum;
-use App\Enums\SegmentEnum;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Student extends Model
+class ClassModel extends Model
 {
     use HasFactory;
 
+    protected $table = 'classes';
     protected $casts = [
-        'segment' => SegmentEnum::class,
         'grade' => GradeEnum::class,
+        'shift' => ShiftEnum::class,
     ];
-
-    public function addresses(): HasMany
-    {
-        return $this->hasMany(Address::class);
-    }
 
     public function enrollments(): HasMany
     {
