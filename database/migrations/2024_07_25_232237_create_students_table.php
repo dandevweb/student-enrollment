04a4f58d660464dad039d64515f077eef06666ff
Danilo Augusto <dandevweb@gmail.com>
2024-07-25 20:29:15 -0300

    feat: creating database structure

---
+++ database/migrations/2024_07_25_232237_create_students_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->date('birth_date');
+            $table->integer('segment');
+            $table->integer('grade');
+            $table->string('father_name');
+            $table->string('mother_name');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ database/migrations/2024_07_25_232515_create_classes_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->string('class_name');
+            $table->integer('shift');
+            $table->integer('vacancies');
+            $table->year('school_year');
+            $table->integer('grade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('classes');
+    }
+};
