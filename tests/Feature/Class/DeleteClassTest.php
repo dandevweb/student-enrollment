5013cb57e0e082515b2316c5ac195b28766f9c20
Danilo Augusto <dandevweb@gmail.com>
2024-07-29 07:49:31 -0300

    test: fix acting as wrong user role

--- tests/Feature/Class/DeleteClassTest.php
+++ tests/Feature/Class/DeleteClassTest.php
@@ -1,13 +1,17 @@
 <?php
 
 use Livewire\Livewire;
-use App\Models\{ClassModel, User};
+use App\Enums\RoleEnum;
 use App\Livewire\Classes\Index;
+
+use App\Models\{ClassModel, User};
 
 use function Pest\Laravel\{actingAs, assertDatabaseMissing};
 
 it('should be able to delete a class', function () {
-    actingAs(User::factory()->create());
+    actingAs(User::factory()->create([
+        'role' => RoleEnum::Secretary,
+    ]));
     $class = ClassModel::factory()->create();
     Livewire::test(Index::class)
         ->set('modelId', $class->id)
